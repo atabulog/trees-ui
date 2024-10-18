915535e27ff99e069dfc45da25d1143c4c307878
Austin Tabulog <austin.tabulog@gmail.com>
2024-10-18 17:37:18 -0500

    added dark mode toggle support for everything except the actual toggle control. That will come later.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {ThemeService} from "./theme-service/theme.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'app';
+  protected darkMode: boolean = true;
+
+  constructor(private themeService: ThemeService) {
+    this.themeService.loadTheme('lara-dark-blue');
+  }
+
+  toggleDarkMode(): void {
+    this.darkMode = !this.darkMode;
+    if(this.darkMode) {
+      this.themeService.loadTheme('lara-dark-blue');
+    }
+    else{
+      this.themeService.loadTheme('lara-light-blue');
+    }
+  }
 }
